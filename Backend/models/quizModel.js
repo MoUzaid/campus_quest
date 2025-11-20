d0f6cce87dfb3b28e397333ae76f1abc67430cae
Mohd Uzaid <mohammeduzaid2@gmail.com>
2025-11-20 15:28:17 +0530

    Done some changes in models

---
+++ Backend/models/quizModel.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    questions: [
+        {
+            questionText: {
+                type: String,
+                required: true,
+            },
+            options: [
+                {
+                    type: String,
+                    required: true,
+                }
+            ],
+            correctAnswer: {
+                type: String,
+                required: true,
+            },
+            marks: {
+                type: Number,
+                required: true,
+                default: 1,
+            },
+            negativeMarks: {
+                type: Number,
+                required: true,
+                default: 0,
+            }
+        }
+    ],
+    totalMarks: {
+        type: Number,
+        required: true,
+    },
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    endTime: {
+        type: Date,
+        required: true,
+    },
+    durationMinutes: {
+        type: Number,
+        required: true,
+    },
+    registeredStudents:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student',
+        }
+],
+    allowedAttempts: {
+        type: Number,
+        required: true,
+        default: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+module.exports = Quiz;
