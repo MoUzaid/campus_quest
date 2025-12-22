4b28c4cca2b34df8e0c003bd4f384ccb416b6715
Mohd Uzaid <mohammeduzaid2@gmail.com>
2025-12-22 16:07:44 +0530

    Added some components

--- Frontend/my-app/src/Pages.jsx
+++ Frontend/my-app/src/Pages.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import CreateQuiz from './pages/CreateQuiz';
+import QuestionsPage from './pages/QuestionsPage';
 
 const Pages = () => {
   return (
     <div>
         <Routes>
         <Route path="/create-quiz" element={<CreateQuiz />} />
-
+        <Route path="/questions" element={<QuestionsPage/>}/>
         </Routes>
     </div>
   )
---
+++ Frontend/my-app/src/redux/app/store.js
@@ -0,0 +1,65 @@
+import { configureStore } from "@reduxjs/toolkit";
+import quizReducer from "../features/quizSlice";
+import courseReducer from "../features/courseSlice";
+import departmentReducer from "../features/departmentSlice";
+
+import { quizApi } from "../services/quizApi";
+import { courseApi } from "../services/coursesApi";
+import { departmentApi } from "../services/departmentApi";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+/* ✅ Persist only quiz slice */
+const persistConfig = {
+  key: "quiz",
+  storage,
+};
+
+const persistedQuizReducer = persistReducer(
+  persistConfig,
+  quizReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    quiz: persistedQuizReducer,
+    course: courseReducer,
+    department: departmentReducer,
+
+    [quizApi.reducerPath]: quizApi.reducer,
+    [courseApi.reducerPath]: courseApi.reducer,
+    [departmentApi.reducerPath]: departmentApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+        ],
+      },
+    }).concat(
+      quizApi.middleware,
+      courseApi.middleware,
+      departmentApi.middleware
+    ),
+
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
